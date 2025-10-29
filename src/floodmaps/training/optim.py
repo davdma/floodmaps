54818c4c65b824364bb1c57057bad2ab5cdfaba0
David Ma <dma@anl.gov>
2025-10-29 09:44:45 -0500

    feat(training): Added AdamW optimizer with weight decay
    For less bells and whistles, first tune Adam and later
    come back and tweak AdamW to see if we can get better
    results through weight decay regularization.

--- src/floodmaps/training/optim.py
+++ src/floodmaps/training/optim.py
@@ -5,6 +5,8 @@ def get_optimizer(model, cfg):
         optimizer = torch.optim.Adam(model.parameters(), lr=cfg.train.lr)
     elif cfg.train.optimizer == 'SGD':
         optimizer = torch.optim.SGD(model.parameters(), lr=cfg.train.lr)
+    elif cfg.train.optimizer == 'AdamW':
+        optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.train.lr, weight_decay=cfg.train.weight_decay)
     else:
         raise Exception('Optimizer not found.')
 
